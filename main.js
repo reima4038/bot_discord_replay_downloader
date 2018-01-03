945cea09579969c8f2bbf44ab8ef9947d8f6f116
reima4038 <reima4038@gmail.com>
2018-01-03 21:44:41 +0900

    基本的な機能を一通り実装。

---
+++ main.js
@@ -0,0 +1,50 @@
+const config = require('config');
+const Eris = require('eris');
+const Client = require('node-rest-client').Client;
+const Buffer = require("buffer").Buffer;
+
+const bot = new Eris(config.token);
+
+bot.on("ready", () => {
+    console.log("Ready!");
+});
+
+bot.on("messageCreate", (msg) => {
+    let message = msg.content.trim();
+    console.log("input:" + message);
+    switch (true){
+        case /^!replay[ ]{1}[0-9]+$/.test(message):
+            let matchId = message.split(" ")[1];
+            console.log("matchId: " + matchId);
+            execute(getReplayUrlCommand, msg, matchId);
+            break;
+        default:
+    }
+});
+
+bot.connect();
+
+/**
+ * コマンドを実行する。
+ * @param {実行コマンドのコールバック関数} cmd 
+ */
+function execute(cmd, msg, matchId){
+    cmd(msg, matchId);
+}
+
+const client = new Client();
+const baseUrl = config.baseUrl;
+
+function getReplayUrlCommand(msg, matchId){
+    client.get(encodeURI(baseUrl + "/api/dota/replays/{matchId}/url?matchId=" + matchId), function (data, response) {
+        let url = new Buffer(data).toString();
+        if(url.trim() == ""){
+            bot.createMessage(msg.channel.id, "そのマッチIDのリプレイな、残念じゃがもう見られん。");
+        } else {
+            bot.createMessage(msg.channel.id, "できたけぇ、もっていきんさい。\n" + url);
+        }
+
+    }).on('error', function (err) {
+        bot.createMessage(msg.channel.id, "すまん、上手ういかんかった");
+    }); 
+}
--- src/main.js
+++
@@ -1,5 +0,0 @@
-import {hello} from './sub';
-
-const message = 'Hello World';
-
-console.log(message);
